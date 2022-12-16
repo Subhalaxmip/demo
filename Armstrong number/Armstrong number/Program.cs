04dcc9477c22df059c989a4e7dc41cfecce48897
Subhalaxmip <115062775+Subhalaxmip@users.noreply.github.com>
2022-12-16 17:00:33 +0530

    armstrong

---
+++ Armstrong number/Armstrong number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Armstrong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a ,b,sum=0,temp;
+            Console.WriteLine("enter number");
+            a=int.Parse(Console.ReadLine());
+            temp = a;
+
+            while (a > 0)
+            { 
+            b = a % 10;
+            sum = sum + (b * b * b);
+            a = a / 10;
+            }
+            if(temp==sum)
+            {
+                Console.WriteLine("armstrong number");
+            }
+            else
+            {
+                Console.WriteLine("not an armstrong number");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
